c6b23117e719b889474792eb382a021081f9107d
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-02-21 10:15:48 +0100

    edited js file

--- index.js
+++ index.js
@@ -17,6 +17,28 @@ if (studentDept === 'Arts') {
 } // This are the subjects to take: Englis,Mathematics,Government, Economics,Literature, History.
 
 //? Question 5
+// let pwr;
+// let firstValue;
+// const nearestPowerOf2 = (num) => {
+//   for (let i = 0; i < num; i++) {
+//     pwr = 2 ** i;
+//     if (pwr < num) {
+//       firstValue = pwr;
+//     }
+//   }
+//   // pwr = num - firstValue > firstValue * 2 - num ? firstValue * 2 : firstValue;
+//   if (num - firstValue > firstValue * 2) {
+//     pwr = firstValue * 2;
+//   } else if (num - firstValue < firstValue * 2) {
+//     pwr = firstValue;
+//   } else {
+//     return `Not a valid number.`;
+//   }
+
+//   return `The number ${pwr} is the power of 2 nearest to ${num}`;
+// };
+// console.log(nearestPowerOf2(3)); //The number 2 is the power of 2 nearest to 3
+
 let pwr;
 let firstValue;
 const nearestPowerOf2 = (num) => {
@@ -27,7 +49,7 @@ const nearestPowerOf2 = (num) => {
     }
   }
   // pwr = num - firstValue > firstValue * 2 - num ? firstValue * 2 : firstValue;
-  if (num - firstValue > firstValue * 2) {
+  if (num - firstValue > firstValue * 2 - num) {
     pwr = firstValue * 2;
   } else if (num - firstValue < firstValue * 2) {
     pwr = firstValue;
@@ -37,4 +59,4 @@ const nearestPowerOf2 = (num) => {
 
   return `The number ${pwr} is the power of 2 nearest to ${num}`;
 };
-console.log(nearestPowerOf2(40)); //The number 2 is the power of 2 nearest to 3
+console.log(nearestPowerOf2(3)); //The number 2 is the power of 2 nearest to 3
