f0ec304ff8e1b500a6435ad2352636922b662ae4
Chidinma Ukazu <ukazuchidinma1@gmail.com>
2023-02-21 10:19:18 +0100

    made changes

--- index.js
+++ index.js
@@ -17,27 +17,6 @@ if (studentDept === 'Arts') {
 } // This are the subjects to take: Englis,Mathematics,Government, Economics,Literature, History.
 
 //? Question 5
-// let pwr;
-// let firstValue;
-// const nearestPowerOf2 = (num) => {
-//   for (let i = 0; i < num; i++) {
-//     pwr = 2 ** i;
-//     if (pwr < num) {
-//       firstValue = pwr;
-//     }
-//   }
-//   // pwr = num - firstValue > firstValue * 2 - num ? firstValue * 2 : firstValue;
-//   if (num - firstValue > firstValue * 2) {
-//     pwr = firstValue * 2;
-//   } else if (num - firstValue < firstValue * 2) {
-//     pwr = firstValue;
-//   } else {
-//     return `Not a valid number.`;
-//   }
-
-//   return `The number ${pwr} is the power of 2 nearest to ${num}`;
-// };
-// console.log(nearestPowerOf2(3)); //The number 2 is the power of 2 nearest to 3
 
 let pwr;
 let firstValue;
